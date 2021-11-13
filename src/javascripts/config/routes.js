a31a2f58d5fa632dae4bffee323fd6ecbf547044
Jason Ure <jason.ure@mail.weber.edu>
2021-11-12 21:17:41 -0700

    suggested fixes
    helpful feedback and chages

--- src/javascripts/config/routes.js
+++ src/javascripts/config/routes.js
@@ -1,16 +1,19 @@
 import express from 'express'
 
-import {indexPage} from '../controllers/index'
-import {allMoviesAPI} from '../controllers/movies'
+// import {indexPage} from '../controllers/index'
+// import {allMoviesAPI} from '../controllers/movies'
+import {indexPage} from '../controllers/movies'
+import {allMoviesAPI} from '../controllers/index'
 
 let router = express.Router()
 
 export function configureRoutes(app){
     router.get('/', indexPage)
-    router.get('/', about)
+    // router.get('/', about)
     //this fixes refresh problems
     router.get('/movies*', indexPage)
     router.get('/api/movies', allMoviesAPI)
 
     app.use('/', router)
 }
+
